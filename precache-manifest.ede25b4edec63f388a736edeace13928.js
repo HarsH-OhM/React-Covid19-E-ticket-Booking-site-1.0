baacd849d2de89cac76de0a9cd738675e59069b7
Harsh <gurjarhariom3114@gmil.com>
2020-08-23 07:04:37 +0530

    Updates

---
+++ precache-manifest.ede25b4edec63f388a736edeace13928.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89061011c9354c5686f0ed3a5a2834d8",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/index.html"
+  },
+  {
+    "revision": "a727ee49d85e67a2eba8",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e7f44f3b4551ea099ee",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/css/main.2e251763.chunk.css"
+  },
+  {
+    "revision": "a727ee49d85e67a2eba8",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/js/2.2145fcd5.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/js/2.2145fcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7f44f3b4551ea099ee",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/js/main.b56a1d92.chunk.js"
+  },
+  {
+    "revision": "2afa09e92acee7b6fb23",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/js/runtime-main.646cd3fa.js"
+  },
+  {
+    "revision": "089faf5c6a1a0e38c073b3a69acf64b4",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/airtrav.089faf5c.webp"
+  },
+  {
+    "revision": "3e369ecabb9e03c521ef9147cf0f95f3",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/backf.3e369eca.jpg"
+  },
+  {
+    "revision": "506e8d314d396e42095785c4b83bd538",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/backf2.506e8d31.jfif"
+  },
+  {
+    "revision": "3a8d2908d44a864e44f83ac2ff843df8",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/bimg.3a8d2908.jfif"
+  },
+  {
+    "revision": "d219c44a40a447735522d2a48656de55",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/bustrav.d219c44a.jpg"
+  },
+  {
+    "revision": "aec9b9392ebcf2e823372820eb334662",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/cloud2.aec9b939.jpg"
+  },
+  {
+    "revision": "c02de3a3366749c955d57f3d6353dec0",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/cold2.c02de3a3.jpg"
+  },
+  {
+    "revision": "cb3223c468c1625dbf26f68bc46f8aad",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/hot.cb3223c4.jpg"
+  },
+  {
+    "revision": "4732be2b6dd606fc55c970aeaf8ed1c6",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/image.4732be2b.jpg"
+  },
+  {
+    "revision": "0ab4ab651ecaff3c3d89e31ab71da407",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/mine.0ab4ab65.jpg"
+  },
+  {
+    "revision": "2f119a31e5b49bf25799b81ada7fc6a2",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/traintrav.2f119a31.jpg"
+  },
+  {
+    "revision": "5244711d2576facd466ed8c2a2acf308",
+    "url": "/React-Covid19-E-ticket-Booking-site-1.0/static/media/travel.5244711d.jfif"
+  }
+]);
